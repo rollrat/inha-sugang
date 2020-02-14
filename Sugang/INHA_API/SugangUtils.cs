d213c7b090ca4cd04023d73095d94d374f4945f8
rollrat <rollrat@inha.edu>
2020-02-14 11:47:04 +0900

    tt: Implements LoadCurrentSeasonSubjects

--- Sugang/INHA_API/SugangUtils.cs
+++ Sugang/INHA_API/SugangUtils.cs
@@ -13,6 +13,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Sugang.INHA_API
@@ -22,6 +23,7 @@ namespace Sugang.INHA_API
         public string Hacksu;
         public string Group;
         public string Name;
+        public string Class;
         public string Score;
         public string Type;
         public string Time;
@@ -78,7 +80,71 @@ namespace Sugang.INHA_API
             var wc = new WebClient();
             var html = wc.DownloadString(url);
 
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+            var root_node = document.DocumentNode;
+
+            var dept = new List<(string, string, bool)>();
+
+            foreach (var node in root_node.SelectNodes("//select[@name='ddlDept']/option"))
+                dept.Add((node.GetAttributeValue("value", ""), node.InnerText.Trim(), true));
+            foreach (var node in root_node.SelectNodes("//select[@name='ddlKita']/option"))
+                dept.Add((node.GetAttributeValue("value", ""), node.InnerText.Trim(), false));
+
             var result = new List<Subject>();
+
+            var form = root_node.SelectSingleNode("//form[@name='form1']");
+
+            Parallel.ForEach(dept, dd =>
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+
+                request.Method = "POST";
+                request.Referer = url;
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0";
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate, br");
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "ko,en-US;q=0.7,en;q=0.3");
+                request.Headers.Add("Upgrade-Insecure-Requests", "1");
+                request.Host = "sugang.inha.ac.kr";
+
+                var request_stream = new StreamWriter(request.GetRequestStream());
+                request_stream.Write($@"itisWebCommonPath=%2FITISWebCommon&itisExternalLinkSite=http%3A%2F%2Fsugang.inha.ac.kr&reportRootPath=%2FITISWebCommon%2Freport&htxtExportType=EXCEL&winClosed=open&errorMessage=&informationMessage=&confirmMessage=&confirmMessageSecu=&informationLeft=&__EVENTTARGET={(dd.Item3 ? "ddlDept" : "ddlKita")}&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE={Uri.EscapeDataString(form.SelectSingleNode("./input[@name='__VIEWSTATE']").GetAttributeValue("value", ""))}&_VIEWSTATEGENERATOR=C62F3353&__EVENTVALIDATION={Uri.EscapeDataString(form.SelectSingleNode("./input[@name='__EVENTVALIDATION']").GetAttributeValue("value", ""))}&ddlDept={(dd.Item3 ? dd.Item1 : "0194002")}&ddlKita={(dd.Item3 ? "4" : dd.Item1)}&ddlTime1=%BC%B1%C5%C3&ddlTime2=%BC%B1%C5%C3&ddlTime3=%BC%B1%C5%C3&rdoKwamokGubun=99&mb_search=&hhdGetval=E-Learning+%B0%AD%C0%C7%BD%C3%B0%A3%C7%A5+%B9%D7+%B0%AD%C0%C7%B0%E8%C8%B9%BC%AD%7C4%7C000&hhdSrchGubun=search2&hhdPopUpState=&hhdCallPage=&hhdCacheTime=&hidLang=KOR&hhdhaksubunban=");
+                request_stream.Close();
+
+                //
+                //  Create Session
+                //
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    var res = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(51949)).ReadToEnd();
+                    var regex = new Regex(@"<td class=""Center"">.*?([A-Z]{3}[0-9]{4})\-([0-9]{3}).*?Center"">.*?</td>.*?Center"">(.*?)</td>.*?Center"">(.*?)</td>.*?Center"">(.*?)</td>.*?Center"">(.*?)</td>.*?Center"">(.*?)</td>.*?Center"">(.*?)</td>.*?Center"">(.*?)</td>.*?Center"">(.*?)</td>");
+                    var match = regex.Match(Regex.Replace(res, "&nbsp;", " "));
+                    var mresult = new List<Subject>();
+                    while (match.Success)
+                    {
+                        var ss = new Subject();
+                        ss.Department = dd.Item2;
+                        ss.Hacksu = match.Groups[1].Value;
+                        ss.Group = match.Groups[2].Value;
+                        ss.Name = match.Groups[3].Value;
+                        ss.Class = match.Groups[4].Value;
+                        ss.Score = match.Groups[5].Value;
+                        ss.Type = match.Groups[6].Value;
+                        ss.Time = match.Groups[7].Value;
+                        ss.Professor = match.Groups[8].Value;
+                        ss.Estimation = match.Groups[9].Value;
+                        ss.Bigo = match.Groups[10].Value;
+                        mresult.Add(ss);
+                        match = match.NextMatch();
+                    }
+
+                    lock(result)
+                        result.AddRange(mresult);
+                }
+            });
 
             return result;
         }
